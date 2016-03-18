c6d071d27f0d4112da3457d99b50da55df4448ac
zrohde <zack.rohde@nbcuni.com>
2016-03-18 12:23:00 -0700

    Adding unregister

--- scripts/yum_update.bash
+++ scripts/yum_update.bash
@@ -2,8 +2,10 @@
 #katelloCACerts
 rpm  -ihv http://newa1katcadmz01.west.nbcnd.aws/pub/katello-ca-consumer-latest.noarch.rpm
 #subscriptionManagerReg
-subscription-manager register --name=newsa1mpxgo01.west.nbcnd.aws --org="NBCND" --activationkey="Production_7_2-UHJvZHVjdGlvbl83XzItCg" --force
+subscription-manager register --name=$(hostname -f ) --org="NBCND" --activationkey="Production_7_2-UHJvZHVjdGlvbl83XzItCg" --force
 #subscriptionManagerConfig
 subscription-manager config --rhsm.baseurl="https://newa1katdmz01.west.nbcnd.aws/pulp/repos"
 #yumUpgrade
 yum -y upgrade
+#unregister from subscription manager
+subscription-manager unregister
