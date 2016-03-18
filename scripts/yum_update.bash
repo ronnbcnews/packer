5a09f15f49e8d6e855d7a75ced941ce008cca80a
zrohde <zack.rohde@nbcuni.com>
2016-03-17 21:49:46 -0700

    Converting cloud init scripts to bash

--- scripts/yum_update.bash
+++ scripts/yum_update.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
- - cloud-init-per once katelloCACerts rpm  -ihv http://newa1katcadmz01.west.nbcnd.aws/pub/katello-ca-consumer-latest.noarch.rpm
- - cloud-init-per once subscriptionManagerReg subscription-manager register --name=newsa1mpxgo01.west.nbcnd.aws --org="NBCND" --activationkey="Production_7_2-UHJvZHVjdGlvbl83XzItCg" --force
- - cloud-init-per once subscriptionManagerConfig subscription-manager config --rhsm.baseurl="https://newa1katdmz01.west.nbcnd.aws/pulp/repos"
- - cloud-init-per always yumUpgrade yum -y upgrade
+#katelloCACerts
+rpm  -ihv http://newa1katcadmz01.west.nbcnd.aws/pub/katello-ca-consumer-latest.noarch.rpm
+#subscriptionManagerReg
+subscription-manager register --name=newsa1mpxgo01.west.nbcnd.aws --org="NBCND" --activationkey="Production_7_2-UHJvZHVjdGlvbl83XzItCg" --force
+#subscriptionManagerConfig
+subscription-manager config --rhsm.baseurl="https://newa1katdmz01.west.nbcnd.aws/pulp/repos"
+#yumUpgrade
+yum -y upgrade
