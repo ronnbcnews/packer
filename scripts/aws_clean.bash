4bdac818dff975db2ae3200bd358b6c655e51946
Ron Calderon <Ron.calderon@nbcuni.com>
2016-03-20 04:26:46 -0400

    merged data from cleanup.inc, since it wont hurt anything if we delete these files

--- scripts/aws_clean.bash
+++ scripts/aws_clean.bash
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-. /tmp/cleanup.inc
+
+rm -vf /etc/udev/rules.d/70-persistent-net.rules
+rm -vf /etc/ssh/*_key
+rm -vf /etc/ssh/*.pub
+rm -vrf /var/lib/puppet/reports/*
+rm -vrf /tmp/*
 rm -vrf /var/lib/cloud/instances/*
 find /var/lib/cloud  /var/lib/puppet/ssl -type f -o -type l -delete
 find /var/log/ -type f -delete
 find /var/tmp/ -type f -delete
 find /var/run/ -type f -delete
-
-
-
