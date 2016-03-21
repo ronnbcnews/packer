5e57a116cde13553db0f5deed7112004be1c10b8
Ron Calderon <Ron.calderon@nbcuni.com>
2016-03-20 20:39:12 -0400

    some more cleanups

--- scripts/aws_clean.bash
+++ scripts/aws_clean.bash
@@ -6,8 +6,12 @@ rm -vf /etc/ssh/*.pub
 rm -vrf /var/lib/puppet/reports/*
 rm -vrf /tmp/*
 rm -vrf /var/lib/cloud/instances/*
+rm -vrf /var/db/sudo/*
+rm -vf /var/db/nscd/*
+rm -vrf /tmp/.*-unix
 find /var/lib/cloud  /var/lib/puppet/ssl -type f -o -type l -delete
 find /var/log/ -type f -delete
 find /var/tmp/ -type f -delete
 find /var/run/ -type f -delete
 find /var/lib/dhclient -type f -exec rm -f '{}' +
+rm -vf /etc/sudoers.d/*
